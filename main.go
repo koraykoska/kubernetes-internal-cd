0efcfce0673ee5a6b780e435cd80d3d8c8875164
Koray Koska <koray@koska.at>
2019-10-28 20:13:27 +0100

    gopls

--- main.go
+++ main.go
@@ -95,6 +95,7 @@ func Webhook(w http.ResponseWriter, r *http.Request) {
 	}
 	globalLogger.Warning(string(secret.Data["master_key"]))
 	globalLogger.Warning(string(secret.Data["master_key_old"]))
+	globalLogger.Warning(hex.EncodeToString(CreateSignature([]byte(body.Data.Github.Repository), secret.Data["master_key"])))
 	hmacSecret := hex.EncodeToString(CreateSignature([]byte(body.Data.Github.Repository), secret.Data["master_key"]))
 	hmacSecretOld := hex.EncodeToString(CreateSignature([]byte(body.Data.Github.Repository), secret.Data["master_key_old"]))
 
